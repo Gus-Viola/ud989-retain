fc83a0d954a35e55438f7be2d88c9ef178759ff3
Gus Viola <gus.viola@pobox.com>
2018-09-17 09:12:34 -0300

    Completed project

--- js/retain.js
+++ js/retain.js
@@ -1,63 +1,72 @@
-$(function(){
+$(function() {
 
-    var model = {
-        init: function() {
-            if (!localStorage.notes) {
-                localStorage.notes = JSON.stringify([]);
-            }
-        },
-        add: function(obj) {
-            var data = JSON.parse(localStorage.notes);
-            data.push(obj);
-            localStorage.notes = JSON.stringify(data);
-        },
-        getAllNotes: function() {
-            return JSON.parse(localStorage.notes);
-        }
-    };
-
-
-    var octopus = {
-        addNewNote: function(noteStr) {
-            model.add({
-                content: noteStr
-            });
-            view.render();
-        },
-
-        getNotes: function() {
-            return model.getAllNotes();
-        },
-
-        init: function() {
-            model.init();
-            view.init();
-        }
-    };
+  var model = {
+    init: function() {
+      if (!localStorage.notes) {
+        localStorage.notes = JSON.stringify([]);
+      }
+    },
+    add: function(obj) {
+      var data = JSON.parse(localStorage.notes);
+      data.push(obj);
+      localStorage.notes = JSON.stringify(data);
+    },
+    getAllNotes: function() {
+      return JSON.parse(localStorage.notes);
+    }
+  };
 
 
-    var view = {
-        init: function() {
-            this.noteList = $('#notes');
-            var newNoteForm = $('#new-note-form');
-            var newNoteContent = $('#new-note-content');
-            newNoteForm.submit(function(e){
-                octopus.addNewNote(newNoteContent.val());
-                newNoteContent.val('');
-                e.preventDefault();
-            });
-            view.render();
-        },
-        render: function(){
-            var htmlStr = '';
-            octopus.getNotes().forEach(function(note){
-                htmlStr += '<li class="note">'+
-                        note.content +
-                    '</li>';
-            });
-            this.noteList.html( htmlStr );
-        }
-    };
+  var octopus = {
+    addNewNote: function(noteStr) {
+      model.add({
+        content: noteStr,
+        date: Date.now()
+        // I added the date above
+      });
+      view.render();
+    },
 
-    octopus.init();
+    getNotes: function() {
+      return model.getAllNotes();
+      // return model.getAllNotes().reverse();
+    },
+
+    init: function() {
+      model.init();
+      view.init();
+    }
+  };
+
+
+  var view = {
+    init: function() {
+      this.noteList = $('#notes');
+      var newNoteForm = $('#new-note-form');
+      var newNoteContent = $('#new-note-content');
+      newNoteForm.submit(function(e) {
+        octopus.addNewNote(newNoteContent.val());
+        newNoteContent.val('');
+        e.preventDefault();
+      });
+      view.render();
+    },
+    render: function() {
+      var htmlStr = '';
+      octopus.getNotes().forEach(function(note) {
+        htmlStr += '<li class="note">' +
+        '<span class="note-date">' + new Date(note.date).toString() + '</span>'+
+          note.content +
+          // '<ul class="date">' + note.date + '</ul>'+
+          '</li>';
+        // htmlStr += '<li class="date">' +
+        //   note.date +
+        //   '</li>';
+
+      });
+      this.noteList.html(htmlStr);
+    }
+  };
+
+  octopus.init();
 });
